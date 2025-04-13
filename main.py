d38f3e4fd40a859545a71bd14ec8bbd954cdec58
LeafyHacker <95765246+LeafyHacker@users.noreply.github.com>
2025-04-13 15:36:48 +0200

    Fertiggestelltes System bis Dato (: V1.0.0

--- main.py
+++ main.py
@@ -1,14 +1,59 @@
 humidity = 0
 temp_raum = 0
-g_temp = 40
+drunter2 = 0
+g_temp = 26
+drunter_alt = 0
 DHT11.set_pin(DigitalPin.P2)
 neZha.set_motor_speed(neZha.MotorList.M4, 0)
 led1 = PlanetX_Display.create(PlanetX_Display.DigitalRJPin.J3, 12, PlanetX_Display.NeoPixelMode.RGB)
 led2 = PlanetX_Display.create(PlanetX_Display.DigitalRJPin.J4, 12, PlanetX_Display.NeoPixelMode.RGB)
+basic.show_number(1)
+def pumpe():
+    global drunter2
+    drunter2 = g_temp - temp_raum
+    if int(g_temp) == temp_raum:
+        neZha.set_motor_speed(neZha.MotorList.M4, 0)
+    elif drunter2 >= 1:
+        neZha.set_motor_speed(neZha.MotorList.M4, 50)
+    elif drunter2 >= 3:
+        neZha.set_motor_speed(neZha.MotorList.M4, 100)
+    elif drunter2 <= -1:
+        neZha.set_motor_speed(neZha.MotorList.M4, -50)
+    elif drunter2 <= -3:
+        neZha.set_motor_speed(neZha.MotorList.M4, -100)
+def leds():
+    global drunter2
+    global drunter_alt
+    # drunter_alt = drunter
+    # drunter = 3
+    # drunter = g_temp - temp_raum
+    if drunter2 != drunter_alt:
+        if drunter2 == 0:
+            led1.show_color(PlanetX_Display.colors(PlanetX_Display.NeoPixelColors.GREEN))
+            led1.show()
+            led2.show_color(PlanetX_Display.colors(PlanetX_Display.NeoPixelColors.GREEN))
+            led2.show()
+        if drunter2 > 0:
+            led1.show_rainbow(330, 400)
+            led2.clear()
+            led2.show()
+            
+        if drunter2 < 0:
+            led2.show_rainbow(200, 300)
+            led1.clear()
+            led1.show()
+    if drunter2 >= 1:
+        led1.rotate(1)
+        led1.show()
+        basic.show_arrow(ArrowNames.EAST, 0)
+    if drunter2 < 0:
+        led2.rotate(-1)
+        led2.show()
+        basic.show_arrow(ArrowNames.WEST, 0)
+    drunter_alt = drunter2
 
 def on_forever():
-    global temp_raum
-    global humidity
+    global humidity, temp_raum
     humidity = DHT11.humidity()
     temp_raum = DHT11.temperature()
     PlanetX_Display.show_user_text(1, "Raumtemperatur:")
@@ -16,32 +61,9 @@ def on_forever():
     PlanetX_Display.show_user_text(5, "Luftfeuchtigkeit:")
     PlanetX_Display.show_user_number(8, humidity)
     pumpe()
-    ledv()
 basic.forever(on_forever)
 
-def pumpe():
-    drunter = g_temp - temp_raum
-    if int(g_temp) == temp_raum:
-        neZha.set_motor_speed(neZha.MotorList.M4, 0)
-    elif drunter >= 3:
-        neZha.set_motor_speed(neZha.MotorList.M4, 50)
-    elif drunter >= 5:
-        neZha.set_motor_speed(neZha.MotorList.M4, 100)
-    elif drunter <= -1:
-        neZha.set_motor_speed(neZha.MotorList.M4, -50)
-    elif drunter <= -3:
-        neZha.set_motor_speed(neZha.MotorList.M4, -100)
-
-def ledv():
-    drunter = g_temp - temp_raum
-    if drunter == 0:
-        led1.clear()
-        led2.clear()
-    if drunter >= 1:
-        led2.clear()
-        led1.rotate(1)
-        led1.show_rainbow(1, 360)
-    if drunter < 0:
-        led1.clear()
-        led2.rotate(1)
-        led2.show_rainbow(1, 360)
+def on_forever2():
+    leds()
+    basic.pause(100)
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,12 +1,74 @@
 let humidity = 0
 let temp_raum = 0
-let g_temp = 40
+let drunter2 = 0
+let g_temp = 26
+let drunter_alt = 0
 DHT11.setPin(DigitalPin.P2)
 neZha.setMotorSpeed(neZha.MotorList.M4, 0)
 let led1 = PlanetX_Display.create(PlanetX_Display.DigitalRJPin.J3, 12, PlanetX_Display.NeoPixelMode.RGB)
 let led2 = PlanetX_Display.create(PlanetX_Display.DigitalRJPin.J4, 12, PlanetX_Display.NeoPixelMode.RGB)
-basic.forever(function on_forever() {
+basic.showNumber(1)
+function pumpe() {
     
+    drunter2 = g_temp - temp_raum
+    if (Math.trunc(g_temp) == temp_raum) {
+        neZha.setMotorSpeed(neZha.MotorList.M4, 0)
+    } else if (drunter2 >= 1) {
+        neZha.setMotorSpeed(neZha.MotorList.M4, 50)
+    } else if (drunter2 >= 3) {
+        neZha.setMotorSpeed(neZha.MotorList.M4, 100)
+    } else if (drunter2 <= -1) {
+        neZha.setMotorSpeed(neZha.MotorList.M4, -50)
+    } else if (drunter2 <= -3) {
+        neZha.setMotorSpeed(neZha.MotorList.M4, -100)
+    }
+    
+}
+
+function leds() {
+    
+    
+    //  drunter_alt = drunter
+    //  drunter = 3
+    //  drunter = g_temp - temp_raum
+    if (drunter2 != drunter_alt) {
+        if (drunter2 == 0) {
+            led1.showColor(PlanetX_Display.colors(PlanetX_Display.NeoPixelColors.Green))
+            led1.show()
+            led2.showColor(PlanetX_Display.colors(PlanetX_Display.NeoPixelColors.Green))
+            led2.show()
+        }
+        
+        if (drunter2 > 0) {
+            led1.showRainbow(330, 400)
+            led2.clear()
+            led2.show()
+        }
+        
+        if (drunter2 < 0) {
+            led2.showRainbow(200, 300)
+            led1.clear()
+            led1.show()
+        }
+        
+    }
+    
+    if (drunter2 >= 1) {
+        led1.rotate(1)
+        led1.show()
+        basic.showArrow(ArrowNames.East, 0)
+    }
+    
+    if (drunter2 < 0) {
+        led2.rotate(-1)
+        led2.show()
+        basic.showArrow(ArrowNames.West, 0)
+    }
+    
+    drunter_alt = drunter2
+}
+
+basic.forever(function on_forever() {
     
     humidity = DHT11.humidity()
     temp_raum = DHT11.temperature()
@@ -15,42 +77,8 @@ basic.forever(function on_forever() {
     PlanetX_Display.showUserText(5, "Luftfeuchtigkeit:")
     PlanetX_Display.showUserNumber(8, humidity)
     pumpe()
-    ledv()
 })
-function pumpe() {
-    let drunter = g_temp - temp_raum
-    if (Math.trunc(g_temp) == temp_raum) {
-        neZha.setMotorSpeed(neZha.MotorList.M4, 0)
-    } else if (drunter >= 3) {
-        neZha.setMotorSpeed(neZha.MotorList.M4, 50)
-    } else if (drunter >= 5) {
-        neZha.setMotorSpeed(neZha.MotorList.M4, 100)
-    } else if (drunter <= -1) {
-        neZha.setMotorSpeed(neZha.MotorList.M4, -50)
-    } else if (drunter <= -3) {
-        neZha.setMotorSpeed(neZha.MotorList.M4, -100)
-    }
-    
-}
-
-function ledv() {
-    let drunter = g_temp - temp_raum
-    if (drunter == 0) {
-        led1.clear()
-        led2.clear()
-    }
-    
-    if (drunter >= 1) {
-        led2.clear()
-        led1.rotate(1)
-        led1.showRainbow(1, 360)
-    }
-    
-    if (drunter < 0) {
-        led1.clear()
-        led2.rotate(1)
-        led2.showRainbow(1, 360)
-    }
-    
-}
-
+basic.forever(function on_forever2() {
+    leds()
+    basic.pause(100)
+})
